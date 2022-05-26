dffc20fc81e981aab2ef264d3c2de866cc924a62
s-da-sailor <akashlanard07@gmail.com>
2022-05-26 15:05:54 +0600

    Issue4: Integrate with Backend and Fetch Stories From Backend

--- src/components/App.js
+++ src/components/App.js
@@ -5,18 +5,21 @@ import Signup from './pages/Signup';
 import Login from './pages/Login';
 import StoryDetails from './pages/StoryDetails';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { StoryContextProvider } from '../contexts/storyContext';
 
 function App() {
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route exact path="/" element={<Home />}></Route>
-          <Route exact path="/signup" element={<Signup />}></Route>
-          <Route exact path="/login" element={<Login />}></Route>
-          <Route exact path="/stories/:id" element={<StoryDetails />}></Route>
-        </Routes>
-      </Layout>
+      <StoryContextProvider>
+        <Layout>
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route exact path="/signup" element={<Signup />}></Route>
+            <Route exact path="/login" element={<Login />}></Route>
+            <Route exact path="/stories/:id" element={<StoryDetails />}></Route>
+          </Routes>
+        </Layout>
+      </StoryContextProvider>
     </Router>
   );
 }
--- src/components/pages/StoryDetails.js
+++ src/components/pages/StoryDetails.js
@@ -1,3 +1,49 @@
+import classes from '../../styles/StoryDetails.module.css';
+import React, { useState, useEffect } from 'react';
+import { useStoryContext } from '../../contexts/storyContext';
+import { useParams } from 'react-router-dom';
+
 export default function StoryDetails() {
-  return <div>Story Details</div>;
+  const { id } = useParams();
+  const { findStoryById } = useStoryContext();
+  const [story, setStory] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const getStory = async () => {
+      try {
+        const singleStory = await findStoryById(id);
+        setStory(singleStory.data.data);
+        setLoading(false);
+      } catch (err) {
+        console.error(err);
+        setLoading(false);
+        setError(true);
+      }
+    };
+    getStory();
+  }, [findStoryById, id]);
+
+  return (
+    <div>
+      {!loading && story && (
+        <div className={classes.story}>
+          <h2 className={classes.title}>{story.title}</h2>
+          <br />
+          <p className={classes.author}>Author: {story.author}</p>
+          <br />
+          <div className={classes.qmeta}>
+            <p className={classes.createdAt}>Created: {new Date(story.createdAt).toUTCString()}</p>
+            <p className={classes.updatedAt}>Last Updated: {new Date(story.updatedAt).toUTCString()}</p>
+          </div>
+          <br />
+          <p className={classes.description}>{story.description}</p>
+        </div>
+      )}
+      {!loading && !story && <div>No story found!</div>}
+      {error && <div>There was an error!</div>}
+      {loading && <div>Loading...</div>}
+    </div>
+  );
 }
